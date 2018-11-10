9a818dd12301a8cfa7ffc33211bf9a55a0cd2f72
Holger Kruusla <holgerkruusla@gmail.com>
2018-11-10 13:37:10 +0200

    veel

--- setup.cmd
+++
@@ -1,7 +0,0 @@
-docker pull mysql/mysql-server:8.0
-docker images
-docker run --name mysql -d mysql/mysql-server:8.0
-docker ps
-docker logs rehealune
-
-docker run --name osayhing -d -p 8000:5000 --rm --link mysql:dbserver rehealune/osayhing:latest
